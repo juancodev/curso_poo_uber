68ebb589e8b3550f899ea24d424cb926b7f70771
juancodev <montillasanchezjuancarlos@gmail.com>
2023-09-19 01:57:14 -0400

    [⚡ADD] Herencia with JavaScript

--- JS/es6/index.js
+++ JS/es6/index.js
@@ -2,7 +2,11 @@
 
 import Car from './Car';
 import Account from './Account';
+import UberX from './uberX';
 
 const carES6 = new Car("QWE123", new Account("Juan Carlos", "V263"));
 carES6.passenger = 4;
 carES6.printDataCar();
+
+const uberX = new UberX("ABC123", new Account("Juan Carlos", "V263"), "Toyota", "Corolla")
+uberX.printDataCar();
